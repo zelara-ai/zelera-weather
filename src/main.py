f5f2ab9489b53222ca72bc5b1d07de69538dfa3a
shafwanur <shafwanur.kazi@gmail.com>
2024-09-11 22:06:57 +0200

    added /weather and /air_pollution endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
 from pymongo import MongoClient
 from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv
 import os
+import requests
 
+load_dotenv()
 app = FastAPI()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+if not API_KEY:
+    raise HTTPException(status_code=500, detail="API key not found")
 
 def get_db_connection():
     client = MongoClient(os.getenv("MONGO_URL"))
@@ -11,7 +18,7 @@ def get_db_connection():
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Zelara Worker API"}
+    return {"message": "Welcome to the Zelara Weather API!"}
 
 @app.get("/api/data")
 def read_data():
@@ -23,3 +30,40 @@ def read_data():
         raise HTTPException(status_code=404, detail="No data found")
 
     return {"data": rows}
+
+@app.get("/weather")
+def get_city_weather(city: str):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Error fetching weather data")
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code = 500, detail = str(e))
+    
+@app.get("/air_pollution")
+def get_air_polution_index(city: str):
+    lat, lon = -1, -1
+    # fetch latitude and longitude using Geocoding API
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={1}&appid={API_KEY}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            lat = response.json()[0]["lat"]
+            lon = response.json()[0]["lon"]
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Error fetching data")
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code = 500, detail = str(e))
+    
+    url = f"http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat={lat}&lon={lon}&appid={API_KEY}"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Error fetching air pollution data")
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code = 500, detail = str(e))
