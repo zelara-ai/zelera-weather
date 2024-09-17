f553fac18cc111f75bcb436ae50e3d367a26e256
shafwanur <shafwanur.kazi@gmail.com>
2024-09-17 23:22:52 +0200

    data can be added onto the database with a post endpoint.

--- src/main.py
+++ src/main.py
@@ -4,15 +4,48 @@ import requests
 from pymongo import MongoClient
 from fastapi import FastAPI, HTTPException, Request, Depends
 from datetime import datetime 
+from bson import ObjectId
 
 app = FastAPI()
+EPS = 1e-4
 
 def get_db_connection():
     client = MongoClient(os.getenv("MONGO_URL"))
     db = client.zelara_db
     return db
 
-def get_bearer_token(request: Request):
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Zelara Weather API!"}
+
+@app.get("/api/data")  # returns all entries in the database
+def read_data():
+    db = get_db_connection()
+    collection = db["mycollection"]
+    rows = list(collection.find({}))
+
+    if not rows:
+        raise HTTPException(status_code = 404, detail = "Database is empty")
+
+    for row in rows:
+        row["_id"] = str(row["_id"])
+    
+    return {
+        "count": len(rows),
+        "data": rows
+    }
+
+@app.delete("/")
+def delete_all_entries():  # deletes all entries in the database. should NOT be misused.
+    db = get_db_connection()
+    collection = db["mycollection"]
+    result = collection.delete_many({})
+    return {
+        "status_code": 200,
+        "message": f"Deleted {result.deleted_count} documents."
+    }
+
+def get_bearer_token(request: Request):  # extracts bearer token from header created whilst making the API call
     authorization: str = request.headers.get("Authorization")
     if not authorization or not authorization.startswith("Bearer "):
         raise HTTPException(status_code = 401, detail = "Authorization header missing or invalid")
@@ -23,24 +56,7 @@ def get_bearer_token(request: Request):
     
     return token
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Zelara Weather API!"}
-
-@app.get("/api/data")  # appends whatever exists in db-fixtures/fixture.json into the database, returns all entries in the database
-def read_data():
-    db = get_db_connection()
-    collection = db["mycollection"]
-    rows = list(collection.find({}))
-
-    if not rows:
-        raise HTTPException(status_code = 404, detail = "No data found")
-
-    return {"data": rows}
-
-@app.get("/weather")  # get weather of one specific city
-def get_weather_data(city: str, api_key: str = Depends(get_bearer_token)):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+def get_response(url):  # a function to handle default behavior when making API calls to openweather
     try:
         response = requests.get(url, timeout = 10)
         response.raise_for_status()
@@ -49,68 +65,69 @@ def get_weather_data(city: str, api_key: str = Depends(get_bearer_token)):
         raise HTTPException(status_code = response.status_code, detail = str(http_err))
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code = 500, detail = str(e))
-
-def get_geolocation(city: str, api_key: str):
+    
+def get_geolocation(city: str, api_key: str):  # getting latitude and longitude values from a city name 
     lat, lon = -1, -1
     url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}"
-    try:
-        response = requests.get(url, timeout = 10)
-        response.raise_for_status()
-        geo_data = response.json()
-        if not geo_data:
-            raise HTTPException(status_code=404, detail="City not found")
-        lat = geo_data[0]["lat"]
-        lon = geo_data[0]["lon"]
-    except requests.exceptions.HTTPError as http_err:
-        raise HTTPException(status_code=response.status_code, detail=str(http_err))
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code = 500, detail = str(e))
-
+    geo_data = get_response(url)
+    lat = geo_data[0]["lat"]
+    lon = geo_data[0]["lon"]
     return (lat, lon)
 
-@app.get("/air_pollution")  # get air quality data of a specific city. latitude and longitude values have to be provided
-def get_air_pollution_index(city: str, api_key: str = Depends(get_bearer_token)):
-    lat, lon = get_geolocation(city, api_key)
-    # Fetch air pollution data
-    url = f"http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat={lat}&lon={lon}&appid={api_key}"
-    try:
-        response = requests.get(url, timeout=10)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as http_err:
-        raise HTTPException(status_code = response.status_code, detail = str(http_err))
-    except requests.exceptions.RequestException as e:
-        raise HTTPException(status_code = 500, detail = str(e))
+@app.get("/find/city")  # find if a city already exists in the database
+def find_city(name: str, lat = None, lon = None, api_key: str = Depends(get_bearer_token)):
+    if lat == None and lon == None: 
+        lat, lon = get_geolocation(name, api_key)  
 
-@app.get("/update")  # update a user's location and/or update their weather if more than 24 hours have passed since the last change
-def update(_id: int, location: str, api_key: str = Depends(get_bearer_token)):
-    db = get_db_connection()
-    collection = db["mycollection"]
-    current_datetime = datetime.now().strftime('%Y-%m-%d')
-
-    rows = collection.find({"_id": _id})
-    for row in rows:
-        if location != row["location"] or abs((datetime.strptime(current_datetime, '%Y-%m-%d') - datetime.strptime(row["last_updated"], '%Y-%m-%d'))).days >= 1:
-            weather_data = get_weather_data(location, api_key)
-            collection.update_one(row, {"$set": {"location": location, "weather_data": weather_data, "last_updated": current_datetime}})
-            return {"success": "Updated successfully!"}
-        
-    return {"data": "0 updates done successfully"}
-
-@app.get("/bulk_refresh")  # bulk refresh everything, egal ob es vor 24 Stunden gemacht wurde oder ned
-def bulk_refresh(api_key: str = Depends(get_bearer_token)):
     db = get_db_connection()
     collection = db["mycollection"]
     rows = list(collection.find({}))
-    current_datetime = datetime.now().strftime('%Y-%m-%d')
     for row in rows: 
-        print(row)
-        weather_data = get_weather_data(row["location"], api_key)
-        collection.update_one(row, {"$set": {"weather_data": weather_data, "last_updated": current_datetime}})
+        geolocation_object = row["weather_data"]["coord"]
+        if abs(geolocation_object["lat"] - lat) < EPS and abs(geolocation_object["lon"] - lon) < EPS:
+            return {
+                "status_code": 200,
+                "id": str(row["_id"])
+            }
+    
+    return {
+        "status_code": 404,
+        "message": "City doesn't exist in the database."
+    }
 
-    rows = list(collection.find({}))
+@app.get("/find/id")  # find id of an existing city in the database
+def find_id(id: str):
+    db = get_db_connection()
+    collection = db["mycollection"]
+    row = collection.find_one({"_id": ObjectId(id)})
+    if row:
+        row["_id"] = str(row["_id"])
+        return {
+            "status_code": 200,
+            "data": row
+        }
 
-    if not rows:
-        raise HTTPException(status_code = 404, detail = "No data found")
+    return {
+        "status_code": 404,
+        "detail": f"_id {id} does not exist in the database"
+    }
 
-    return {"data": rows}
+@app.post("/add")  # add a new city to the database.  
+def add_city(city: str, api_key: str = Depends(get_bearer_token)):
+    lat, lon = get_geolocation(city, api_key) 
+    if find_city(city, lat, lon)["status_code"] == 200:
+            raise HTTPException(status_code = 404, detail = "City already exists in the database.")
+
+    db = get_db_connection()
+    collection = db["mycollection"]
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    weather_data = get_response(url)
+    data = {
+        "weather_data": weather_data,
+        "last_updated": datetime.now().strftime("%d.%m.%Y")
+    }
+    result = collection.insert_one(data)
+    return {
+        "status_code": "200",
+        "message": f"Successfully added object with id {result.inserted_id}"
+    }
